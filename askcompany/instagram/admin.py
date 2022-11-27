bcfcaf341e86a2601832008e577e4a1f82f6fffd
JustinChoiHokyeong <chhk7449@gmail.com>
2022-11-28 00:10:00 +0900

    models practice

--- askcompany/instagram/admin.py
+++ askcompany/instagram/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# admin.site.register(Post)
+
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+
+# admin.site.register(Post, PostAdmin)
+# 특정 모델에 대해서 어드민 등록은 한번만 가능하다
+
+@admin.register(Post) #파이썬 장식자 문법으로 사용, Wrapping, 감싼 대상의 기능을 변경하는 것이다.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'message_length','is_public' , 'created_at', 'updated_at']
+    list_display_links = ['message']
+    list_filter = ['created_at', 'is_public']
+    search_fields = ['message']
+
+    def message_length(self, post):
+        # return len(post.message)
+        # return f"{len(post.message)} 글자"
+        return "{} 글자".format(len(post.message))
--- askcompany/instagram/models.py
+++ askcompany/instagram/models.py
@@ -4,6 +4,18 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
+    #verbose_name : 읽기쉬운 모델 객체의 이름 표시, 옵션을 지정하지 않으면 클래스 이름이 소문자로 변경된 것으로 표시된다
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    #Java의 toString
+    def __str__(self):
+        # return "Custom Post object ({})".format(self.id)
+        return self.message
+
+    # def message_length(self):
+    #     return len(self.message)
+    # message_length.short_description = "메시지 글자수"
+
     
